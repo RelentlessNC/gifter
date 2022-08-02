5c0d64bb07df745aa8efc715a0c5be5adcca4607
RelentlessNC <njconklin1987@gmail.com>
2022-08-01 19:01:38 -0500

    commit cause i have to.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,72 +20,72 @@ var settings = {
     // ModelChange: model //(array of objects) new data object to pass into calendar (serving suggestion: passing through only the currently selected month's events if working with large dataset.
 }
 
-$( function() {
-    $( "#accordion" ).accordion();
-  } );
+$(function() {
+    $("#accordion").accordion();
+});
 
 //event listener for Add Event - toggles hide for event modal box
 $(".add-event").on('click', function() {
     eventModal.classList.toggle('hide')
 })
 
+
+
 //event listener for Schedule Event button - toggles hide for event modal box
 $(".schedule-button").on('click', function(e) {
     e.preventDefault()
     eventModal.classList.toggle('hide')
-    $("body").css("background-color","gray");
-    $("#confirmation-message").css("visibility","visible");
+    $("body").css("background-color", "gray");
+    $("#confirmation-message").css("visibility", "visible");
     // script for confirmation modal w/ jquery
-    $(function () {
+    $(function() {
         $("#confirmation-message").dialog({
-                modal: true,
-                resizable: false,
-                buttons: [
-                    {
-                        text: "Confirm",
-                        "class": 'btn-hover color-3 modalButtonClass',
-                        // script for congrats modal when confirm is clicked
-                        click: function() {
-                            $( this ).dialog( "close" );
-                            $("#congrats-message").css("visibility","visible");
-                            $(function () {
-                                $("#congrats-message").dialog({
-                                        modal: true,
-                                        width: 500,
-                                        resizable: false,
-                                        buttons: [
-                                            {
-                                                text: "Back to Calendar",
-                                                "class": 'btn-hover color-3 modalButtonClass',
-                                                "id": 'backBtn',
-                                                click: function() {
-                                                    $( this ).dialog( "close" );
-                                                    $("body").css("background-color","transparent");
-                                                }
-                                            },
-                                            {
-                                                text: "Schedule Another Event",
-                                                "class": 'btn-hover color-3 modalButtonClass',
-                                                "id": 'scheduleAnotherBtn',
-                                                click: function() {
-                                                    $( this ).dialog( "close" );
-                                                    $("body").css("background-color","transparent");
-                                                }
-                                            }
-                                        ]
-                                    })
-                                })
-                            }
-                    },
-                    {
-                        text: "Cancel",
-                        "class": 'btn-hover color-3 modalButtonClass',
-                        click: function() {
-                            $( this ).dialog( "close" );
-                            $("body").css("background-color","transparent");
-                        }
+            modal: true,
+            resizable: false,
+            buttons: [{
+                    text: "Confirm",
+                    "class": 'btn-hover color-3 modalButtonClass',
+                    // script for congrats modal when confirm is clicked
+                    click: function() {
+                        $(this).dialog("close");
+                        $("#congrats-message").css("visibility", "visible");
+                        $(function() {
+                            $("#congrats-message").dialog({
+                                modal: true,
+                                width: 500,
+                                resizable: false,
+                                buttons: [{
+                                        text: "Back to Calendar",
+                                        "class": 'btn-hover color-3 modalButtonClass',
+                                        "id": 'backBtn',
+                                        click: function() {
+                                            $(this).dialog("close");
+                                            $("body").css("background-color", "transparent");
+                                        }
+                                    },
+                                    {
+                                        text: "Schedule Another Event",
+                                        "class": 'btn-hover color-3 modalButtonClass',
+                                        "id": 'scheduleAnotherBtn',
+                                        click: function() {
+                                            $(this).dialog("close");
+                                            $("body").css("background-color", "transparent");
+                                        }
+                                    }
+                                ]
+                            })
+                        })
                     }
-                ]
-            })
+                },
+                {
+                    text: "Cancel",
+                    "class": 'btn-hover color-3 modalButtonClass',
+                    click: function() {
+                        $(this).dialog("close");
+                        $("body").css("background-color", "transparent");
+                    }
+                }
+            ]
         })
+    })
 })
