c8b629ff22b6d966bd06eb9f8ef9c7813d8bf029
CNRBanks <connortamminen@gmail.com>
2022-08-04 18:43:03 -0400

    updated js to populate giph once event has been selected

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,49 +1,55 @@
 const eventModal = document.getElementById("dialog-message")
 const editBox = document.getElementById("edit-box")
 const giphyAPI = 'https://api.giphy.com/v1/gifs/search';
+const selectEventTypeEl = document.getElementById('etype');
 const giphyKEY = 'PVW7bT7xE7oiwvc3VLc9oHgGuFdSrfUb';
 const giftbitKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJTSEEyNTYifQ==.MytEdlNFcVp3clFCT2hrZ0Uxb1FNc2pZbWRoRjVKVmYwdlh3L2x6c0hqL1QvYTJpQ1N2cW1kc1JqOEFLWDJTMjJ0cmNzODNaSVVMOGJvcldOWTVNVkJBV1Yvb1B3ck4vZGQyMVNkcE9EN1pSMm8xeFdYbHRwd0ZPaVlsaHB2Smk=.weqw9hjbaEcLpqZlkrVMFngOntTuAIi3d09A/4dybFs=';
 const giftbitAPI = 'https://private-anon-b3a6e921d5-giftbit.apiary-proxy.com/papi/v1/brands';
-var allGiphs = [];
+// var allGiphs = [];
 var allBrands = [];
 var allEvents = [];
-const eventType = 'rabbit'; //document.getElementById('etype').value;
 
 
 
-// Fetch the GIPHY API and retrieve the GIFS
-fetch(giphyAPI + '?q=' + eventType + '&limit=10&api_key=' + giphyKEY)
-    .then(function(response) {
-        if (!response) {
-            console.log('error');
-        }
-        return response.json();
-    })
-    .then(function(data) {
-        allGiphs = data;
-        displayGifs();
+function fetchGifs(e) {
+    // Fetch the GIPHY API and retrieve the GIFS
 
-    })
-    .catch((err) => {});
+    fetch(giphyAPI + '?q=' + e.target.value + '&limit=1&api_key=' + giphyKEY)
+        .then(function (response) {
+            if (!response) {
+                console.log('error');
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            // allGiphs = data;
+            displayGifs(data);
+            console.log(allGiphs)
 
-function displayGifs() {
-    var tab3 = document.getElementById('tabs-3');
+        })
+        .catch((err) => { });
 
-    for (var i = 0; i < allGiphs.data.length; i++) {
+
+}
+
+function displayGifs(gifData) {
+    var tab3 = document.getElementById('test-2');
+    tab3.innerHTML = "";
+    for (var i = 0; i < gifData.data.length; i++) {
         var img = document.createElement('img');
-        img.setAttribute('src', allGiphs.data[i].images.downsized.url);
+        img.setAttribute('src', gifData.data[i].images.fixed_height_small.url);
         tab3.appendChild(img);
     }
 }
 
 // giftbit api
 fetch(giftbitAPI, {
-        headers: {
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJTSEEyNTYifQ==.MytEdlNFcVp3clFCT2hrZ0Uxb1FNc2pZbWRoRjVKVmYwdlh3L2x6c0hqL1QvYTJpQ1N2cW1kc1JqOEFLWDJTMjJ0cmNzODNaSVVMOGJvcldOWTVNVkJBV1Yvb1B3ck4vZGQyMVNkcE9EN1pSMm8xeFdYbHRwd0ZPaVlsaHB2Smk=.weqw9hjbaEcLpqZlkrVMFngOntTuAIi3d09A/4dybFs=',
-        }
-    })
+    headers: {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJTSEEyNTYifQ==.MytEdlNFcVp3clFCT2hrZ0Uxb1FNc2pZbWRoRjVKVmYwdlh3L2x6c0hqL1QvYTJpQ1N2cW1kc1JqOEFLWDJTMjJ0cmNzODNaSVVMOGJvcldOWTVNVkJBV1Yvb1B3ck4vZGQyMVNkcE9EN1pSMm8xeFdYbHRwd0ZPaVlsaHB2Smk=.weqw9hjbaEcLpqZlkrVMFngOntTuAIi3d09A/4dybFs=',
+    }
+})
     .then((response) => response.json())
-    .then(function(data) {
+    .then(function (data) {
         allBrands = data;
         displayBrands();
         console.log(data);
@@ -63,24 +69,24 @@ function displayBrands() {
 }
 
 //tabs jQuery function
-$(function() {
+$(function () {
     $("#tabs").tabs();
 });
 
 //accordion jQuery function
-$(function() {
+$(function () {
     $("#accordion").accordion();
 });
 
 //event listener for Add Event - toggles hide for event modal box
-$(".add-event").on('click', function() {
+$(".add-event").on('click', function () {
 
-    $(function() {
+    $(function () {
         $("#dialog-message").dialog({
             modal: true,
         })
     })
-    $(function() {
+    $(function () {
         $("#dialog-message").dialog({
             modal: true,
         });
@@ -88,12 +94,12 @@ $(".add-event").on('click', function() {
 })
 
 //edit button event listener
-$(".edit-button").on('click', function() {
+$(".edit-button").on('click', function () {
     console.log('hello')
     editBox.classList.toggle('hide')
 })
 
-$("#etype").on('mouseout', function() {
+$("#etype").on('mouseout', function () {
     const selectedOption = $("#etype option:selected").val()
     let otherOption = document.getElementById("other-option")
     if (selectedOption == 'other') {
@@ -104,7 +110,7 @@ $("#etype").on('mouseout', function() {
 })
 
 //event listener for Schedule Event button - toggles hide for event modal box
-$(".schedule-button").on('click', function() {
+$(".schedule-button").on('click', function () {
     const parentNode = $(this)[0].parentNode
     let event = {
         title: parentNode.children[2].value,
@@ -140,52 +146,52 @@ $(".schedule-button").on('click', function() {
     $("body").css("background-color", "gray");
     $("#confirmation-message").css("visibility", "visible");
     // script for confirmation modal w/ jquery
-    $(function() {
+    $(function () {
         $("#confirmation-message").dialog({
             modal: true,
             width: 650,
             resizable: false,
             buttons: [{
-                    text: "Confirm",
-                    "class": 'modalButtonClass',
-                    // script for congrats modal when confirm is clicked
-                    click: function() {
-                        $(this).dialog("close");
-                        $("#congrats-message").css("visibility", "visible");
-                        $(function() {
-                            $("#congrats-message").dialog({
-                                modal: true,
-                                width: 650,
-                                resizable: false,
-                                buttons: [{
-                                        text: "Back to Calendar",
-                                        "class": 'modalButtonClass',
-                                        click: function() {
-                                            $(this).dialog("close");
-                                            $("body").css("background-color", "transparent");
-                                        }
-                                    },
-                                    {
-                                        text: "Schedule Another Event",
-                                        "class": 'modalButtonClass',
-                                        click: function() {
-                                            $(this).dialog("close");
-                                            $("body").css("background-color", "transparent");
-                                        }
-                                    }
-                                ]
-                            })
+                text: "Confirm",
+                "class": 'modalButtonClass',
+                // script for congrats modal when confirm is clicked
+                click: function () {
+                    $(this).dialog("close");
+                    $("#congrats-message").css("visibility", "visible");
+                    $(function () {
+                        $("#congrats-message").dialog({
+                            modal: true,
+                            width: 650,
+                            resizable: false,
+                            buttons: [{
+                                text: "Back to Calendar",
+                                "class": 'modalButtonClass',
+                                click: function () {
+                                    $(this).dialog("close");
+                                    $("body").css("background-color", "transparent");
+                                }
+                            },
+                            {
+                                text: "Schedule Another Event",
+                                "class": 'modalButtonClass',
+                                click: function () {
+                                    $(this).dialog("close");
+                                    $("body").css("background-color", "transparent");
+                                }
+                            }
+                            ]
                         })
-                    }
-                },
-                {
-                    text: "Cancel",
-                    "class": 'modalButtonClass',
-                    click: function() {
-                        $(this).dialog("close");
-                        $("body").css("background-color", "transparent");
-                    }
+                    })
                 }
+            },
+            {
+                text: "Cancel",
+                "class": 'modalButtonClass',
+                click: function () {
+                    $(this).dialog("close");
+                    $("body").css("background-color", "transparent");
+                }
+            }
             ]
         })
     })
@@ -209,11 +215,11 @@ function dateReminder() {
     // for (var i = 0; i < allEvents.length; i++){
     //     if (allEvents[i].date <= (todaysDate.getDate + 14)){
     // DISPLAY REMINDER MODAL
-    $(function() {
+    $(function () {
         $("#reminder-modal").dialog({
             modal: true,
             buttons: {
-                Ok: function() {
+                Ok: function () {
                     $(this).dialog("close");
                 }
             }
@@ -222,3 +228,5 @@ function dateReminder() {
     //     }
     // }
 }
+
+selectEventTypeEl.addEventListener("change", fetchGifs)
